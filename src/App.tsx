806703e9744f802053bd5354fff69c78b5475d8d
bonnina <37539513+bonnina@users.noreply.github.com>
2018-12-29 23:19:11 +0200

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import Circle from './Circle';
 
-class App extends React.Component {
+class App extends React.Component<{}> {
   public render() {
     return (
       <div>
+        <Circle progress={0.4} />
       </div>
     );
   }
