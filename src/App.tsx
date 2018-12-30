e853666c5ec8453fee14932d05cd9a30a8fbc3ae
bonnina <37539513+bonnina@users.noreply.github.com>
2018-12-30 21:33:21 +0200

    Add timer logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,96 @@
 import * as React from 'react';
 import Circle from './Circle';
 
-class App extends React.Component<{}> {
+interface State {
+  session: number;
+  length: number;
+  time: string;
+}
+
+class App extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    
+    this.showTime = this.showTime.bind(this);
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+    this.countdown = this.countdown.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  public state: State = {
+    session: 5,
+    length: 0, 
+    time: "00:00"
+  };
+
+  componentDidMount() {
+    this.showTime();
+  }
+
+  public showTime() {
+    const hrs: number = Math.floor(this.state.session / 60);
+    const min: number = Math.floor(this.state.session) % 60;
+    const sec: number =  Math.floor((this.state.session * 60) % 60);
+    const t: string = (hrs > 0 ? hrs + ":" : "") + (min < 10 ? "0" + min : min) + ":" + (sec < 10 ? "0" + sec : sec);
+    this.setState({ time: t });
+  };
+
+  public async onIncrement() {
+    await this.setState({ session: this.state.session + 1 });
+    this.showTime();
+  };
+
+  public async onDecrement() {
+    if (this.state.session > 1) {
+      await this.setState({ session: this.state.session - 1 });
+      this.showTime();
+    } else return
+  };
+
+  public countdown() {
+    this.setState({ 
+      length: this.state.session * 60
+    });
+
+    let interval = setInterval(() => {
+      this.setState({ length: this.state.length - 1 });
+
+      if (this.state.length <= 0) {
+        clearInterval(interval);
+        this.reset();
+      }
+    }, 1000);
+  };
+
+  public reset() {
+    this.setState({ 
+      session: 0.1,
+      length: 0,
+      time: "00:00"
+    });
+  };
+
+  public wikiRedirect() {
+    window.open("https://en.wikipedia.org/wiki/Pomodoro_Technique");
+  }
+
   public render() {
     return (
       <div>
-        <Circle progress={0.4} />
+        <div className='controls'>
+          <button id='start' onClick={this.countdown}> &#8227; </button>
+          <button id='reset' onClick={this.reset}> &#10006; </button>
+          <button id='about' onClick={this.wikiRedirect}> &#63; </button>
+        </div>
+
+        <Circle progress={ 1 - (this.state.length / 60 / this.state.session) } />
+
+        <div className='clock'>
+          <button onClick={this.onIncrement}> + </button>
+          <p> {this.state.time} </p>
+          <button onClick={this.onDecrement}> - </button>
+        </div>
       </div>
     );
   }
